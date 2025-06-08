bb0a8eb9890f74bea8b1737756987baa618756fa
rexxa <rexxa.som@kakaoenterprise.com>
2025-06-08 16:24:35 +0900

    Update combine_model_config.py

--- app/combine_model_config.py
+++ app/combine_model_config.py
@@ -83,7 +83,7 @@ class CombinedModel(PreTrainedModel):
             loglik = torch.nn.functional.logsigmoid(m1_diag1 * logits_per_text)
             nll = -torch.sum(loglik, dim=-1)
             loss = nll.mean()
-        return BaseModelOutputWithPooling(
+        return BaseModelOutput(
             loss=loss,
             logits_per_image=logits_per_image,
             logits_per_text=logits_per_text,
